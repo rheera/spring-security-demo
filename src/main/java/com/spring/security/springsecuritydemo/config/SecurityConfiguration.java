9cdb891e9fbf053643be03336d251052d94be850
Raman Heera <rheera2@uwo.ca>
2019-04-24 13:04:04 -0400

    Update SecurityConfiguration.java
    - added a user and admin into the authentication manager
    - any request must be done by authenticated users now

--- src/main/java/com/spring/security/springsecuritydemo/config/SecurityConfiguration.java
+++ src/main/java/com/spring/security/springsecuritydemo/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.spring.security.springsecuritydemo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,12 +16,29 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         //if you don't want to authorize all requests you can add antMatchers and add the appropriate urls
         //httpSecurity.authorizeRequests().antMatchers("/rest/hello").denyAll().and().httpBasic();
-        //permit all and any requests and give me all the httpbasic authentacations
-        httpSecurity.authorizeRequests().anyRequest().permitAll().and().httpBasic();
 
-        //disable cross site request forgery for simplicity 
+        //permit all and any requests and give me all the httpbasic authentications
+        //httpSecurity.authorizeRequests().anyRequest().permitAll().and().httpBasic();
+
+        /*now that we have some users in the authentication manager we can allow only the users in the manager
+        we are saying every request should be authenticated
+         */
+        httpSecurity.authorizeRequests().anyRequest().fullyAuthenticated().and().httpBasic();
+
+        //disable cross site request forgery for simplicity
         httpSecurity.csrf().disable();
     }
 
+    /*to authenticate this particular server, give our own usernames and passwords,
+    in memory authentication lets you put the username and password right here
+    we created 2 users for the authentication manager
+     */
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("Raman").password("test").roles("USER").and()
+                .withUser("demo").password("test2").roles("admin");
+    }
 
 }
